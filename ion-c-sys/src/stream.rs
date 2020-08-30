a041e854f00ba17fa6de7aa8d382fa32b5d5f392
Almann Goo <almann@amazon.com>
2020-08-30 10:19:31 -0700

    More progress towards Ion C User Stream Callbacks.

--- ion-c-sys/src/stream.rs
+++ ion-c-sys/src/stream.rs
@@ -5,10 +5,18 @@
 use std::io::BufRead;
 use std::io::Write;
 
+use crate::*;
+
 /// Represents an input stream for Ion C.  
 #[repr(C)]
 struct IonCReadStream<T: BufRead> {
     input: T,
+}
+
+impl<T: BufRead> IonCReadStream<T> {
+    pub fn new(input: T) -> Self {
+        Self { input }
+    }
 }
 
 /// Represents an output stream for Ion C.
@@ -17,6 +25,34 @@ struct IonCWriteStream<T: Write> {
     output: T,
 }
 
-/// Represents an output s
+impl<T: Write> IonCWriteStream<T> {
+    pub fn new(output: T) -> Self {
+        Self { output }
+    }
+}
 
-extern "C" {}
+/// Bridges the raw C interface for the `_ion_user_stream` handler to something a bit more Rust
+/// friendly and can use a closure environment to handle the Rust context generically.
+#[repr(C)]
+pub struct IonCStreamHandler<F: FnMut(&mut _ion_user_stream) -> IonCResult<()>> {
+    handler: F,
+}
+
+impl<F: FnMut(&mut _ion_user_stream) -> IonCResult<()>> IonCStreamHandler<F> {
+    pub fn new(handler: F) -> Self {
+        Self { handler }
+    }
+}
+
+unsafe extern "C" fn ionc_stream_handler<F>(stream: *mut _ion_user_stream) -> iERR
+where
+    F: FnMut(&mut _ion_user_stream) -> IonCResult<()>,
+{
+    let user_stream = stream.as_mut().unwrap();
+    let handler_obj_ptr = user_stream.handler_state as *mut IonCStreamHandler<F>;
+    let result = ((*handler_obj_ptr).handler)(user_stream);
+    match result {
+        Ok(_) => ion_error_code_IERR_OK,
+        Err(e) => e.code,
+    }
+}
